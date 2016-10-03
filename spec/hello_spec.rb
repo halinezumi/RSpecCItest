51a4c33061b39d76236400716f41eeabe71587a0
halinezumi <halinezumi777@gmail.com>
2016-10-03 17:34:45 +0900

    fail case commit

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Hello do
   it "message return hello" do
     expect(Hello.new.message).to eq "hello"
   end
+
+  it "[fail case] message return hello" do
+    expect(Hello.new.message).to eq "hello_fail"
+  end
+
 end
